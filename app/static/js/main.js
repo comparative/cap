ead510dadffc3f1dceeea8a80dca07de15132853
Geoff Boyd <geoff.boyd@austin.utexas.edu>
2015-03-04 12:23:17 +0000

    country homepages 1st pass

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,20 +1,24 @@
 $(document).ready(function() {
+    
+    $("ul.slider li").addClass('slideborder-off');
 
 	$("#updates").focus(function() {
   		
   		$(this).val("");
   		
-  		$('#social').addClass('show-for-large-only');
+  		//$('#social').addClass('show-for-large-only');
   		
-  		$('#email').removeClass('medium-2');
-  		$('#email').addClass('medium-4');
+  		//$('#email').removeClass('medium-2');
+  		//$('#email').addClass('medium-4');
   	
-        $('#updates-container').removeClass('medium-12');
-        $('#updates-container').addClass('medium-8');
+        //$('#updates-container').removeClass('medium-12');
+        //$('#updates-container').addClass('medium-8');
 
-        $('#go-container').addClass('medium-4');
+        //$('#go-container').addClass('medium-4');
         
-        setTimeout(function(){ $('#go-container').show(); }, 500);
+        //setTimeout(function(){ $('#go-container').show(); }, 500);
+        
+        $('#go-container').show();
 
 	});
 	
@@ -28,5 +32,26 @@ $(document).ready(function() {
 		$(this).removeClass('highlight');
 	
 	});
+	
+	
+	$("ul.slider li").hover(function() {
+	    
+	    $(this).removeClass('slideborder-off');
+		$(this).addClass('slideborder');
+		
+		//$(".orbit-caption a").addClass('bold-link');
+		$(".orbit-caption a").show();
+		
+	
+	}, function() {
+	
+		$(this).removeClass('slideborder');
+		$(this).addClass('slideborder-off');
+	    
+	   // $(".orbit-caption a").removeClass('bold-link');
+	   $(".orbit-caption a").hide();
+	
+	});
+		
 	
 });
