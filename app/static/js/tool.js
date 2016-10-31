6c1fe6cd0944c47f7d968c08438852d1fbc32b16
Geoff Boyd <geoff.boyd@austin.utexas.edu>
2016-10-31 13:56:12 +0100

    remove create guid

--- app/static/js/tool.js
+++ app/static/js/tool.js
@@ -1948,14 +1948,6 @@ function getQueryVariable(variable) {
        return(false);
 }
 
-function createGuid()
-{
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-        var r = Math.random()*16|0, v = c === 'x' ? r : (r&0x3|0x8);
-        return v.toString(16);
-    });
-}
-
 function getInstancesUri(filters,dataset,flag,topic,frm,to) {
     
     // have filters been checked?
